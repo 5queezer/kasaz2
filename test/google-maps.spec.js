220b47204307dedb3e414fa9b9910b8752162ba7
Christian Pojoni <dev@pojoni.at>
2020-01-05 18:46:04 +0100

    tests updated

--- test/google-maps.spec.js
+++
@@ -1,3 +0,0 @@
-import { mount } from '@vue/test-utils'
-import plugin from '@/plugins/google-maps'
-import Maps from '@/components/Maps.vue'
--- test/unit/Components.spec.js
+++ test/unit/Components.spec.js
@@ -1,9 +1,25 @@
 import { mount, createLocalVue } from '@vue/test-utils'
 import Bootstrap from 'bootstrap-vue'
 import Logo from '@/components/Logo.vue'
+import Maps from '@/components/Maps.vue'
 
 const localVue = createLocalVue()
 localVue.use(Bootstrap)
+localVue.use(Maps)
+
+describe('Google Maps', () => {
+  const options = {
+    localVue,
+    propsData: {
+      locations: []
+    }
+  }
+
+  test('is a Vue instance', () => {
+    const wrapper = mount(Maps, options)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
 
 describe('Logo', () => {
   const options = { localVue }
