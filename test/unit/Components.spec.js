1ba907228c5bb4cbe267ed1874865d8c1890f614
Christian Pojoni <dev@pojoni.at>
2020-01-05 19:06:42 +0100

    refactor test components

--- test/unit/Components.spec.js
+++ test/unit/Components.spec.js
@@ -1,15 +1,12 @@
 import { mount, createLocalVue } from '@vue/test-utils'
+import VueLodash from 'vue-lodash'
 import Bootstrap from 'bootstrap-vue'
 import Logo from '@/components/Logo.vue'
 import Maps from '@/components/Maps.vue'
-
-const localVue = createLocalVue()
-localVue.use(Bootstrap)
-localVue.use(Maps)
+import Navbar from '@/components/Navbar.vue'
 
 describe('Google Maps', () => {
   const options = {
-    localVue,
     propsData: {
       locations: []
     }
@@ -22,9 +19,26 @@ describe('Google Maps', () => {
 })
 
 describe('Logo', () => {
+  const localVue = createLocalVue()
+  localVue.use(Bootstrap)
+
   const options = { localVue }
   test('is a Vue instance', () => {
     const wrapper = mount(Logo, options)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
+
+describe('Navbar', () => {
+  const localVue = createLocalVue()
+  localVue.use(VueLodash, { name: 'lodash' })
+  localVue.use(Bootstrap)
+
+  const options = {
+    localVue
+  }
+  test('is a Vue instance', () => {
+    const wrapper = mount(Navbar, options)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
