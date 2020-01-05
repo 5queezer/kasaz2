0e3a6bff5bd0f70c16d42d6f545d1712fe570bc4
Christian Pojoni <dev@pojoni.at>
2020-01-05 11:06:35 +0100

    test actions

---
+++ test/unit/store/actions.spec.js
@@ -0,0 +1,18 @@
+import { actions } from '@/store/index'
+import api from '@/store/api'
+jest.mock( '@/store/api' );
+
+describe('store actions', () => {
+  let commit
+
+  beforeEach(() => {
+    commit = jest.fn()
+  })
+
+  it('can fetch', async () => {
+    const resp = [{id:0},{id:1}]
+    api.getApartments.mockResolvedValue(resp)
+    await actions.fetch({commit})
+    expect(commit).toBeCalledWith('set', resp)
+  })
+})
