c062f46fc7bc6e239b8c49df62cb401e161b9642
Christian Pojoni <dev@pojoni.at>
2020-01-07 00:15:47 +0100

    searchbox component

---
+++ components/Searchbox.vue
@@ -0,0 +1,10 @@
+<template>
+  <b-input-group>
+    <b-form-input type="search" placeholder="Location" />
+    <b-input-group-append>
+      <b-button variant="success">
+        <i class="fa fa-search" aria-label="Search" />
+      </b-button>
+    </b-input-group-append>
+  </b-input-group>
+</template>
